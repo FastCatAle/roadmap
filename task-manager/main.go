913d6059d8d11241c0d1e01dc613398c82518f18
FastCatAle <93169985+FastCatAle@users.noreply.github.com>
2024-11-27 23:17:28 -0500

    started with addTask and some TODO's

--- task-manager/main.go
+++ task-manager/main.go
@@ -3,53 +3,71 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"os"
-    "time"
+	"time"
 )
+
+type TaskId int
+type TaskStatus int
+type Task map[TaskId]TaskContent
 
 const (
-    DONE  = "done"
-    IN_PROGRESS  = "in-progress"
-    PENDING = "pending"
+	DONE TaskStatus = iota
+	IN_PROGRESS
+	PENDING
 )
 
-func (t* Task) deleteTask() {
-    /*
-    TODO: logic to delete an existing task
-    by ID.
-    */
-}
-
-func (t* Task) updateTask() {
-    /*
-    TODO: logic to update the status of a update 
-    and change the 'UpdatedAt' time.
-    */
-}
-
-func (t* Task) addTask() {
-    //TODO: logic to add a new task with a new ID
-}
-
-type Task struct {
-	Id          int    `json:"id"`
+type TaskContent struct {
 	Description string `json:"description"`
 	Status      string `json:"status"`
 	CreatedAt   string `json:"createdAt"`
 	UpdatedAt   string `json:"updatedAt"`
 }
 
-func main() {
+func checkTaskStatus(ts TaskStatus) string {
+    var status string
+    switch ts {
+    case DONE:
+        status = "done"
+    case IN_PROGRESS:
+        status = "in-progress"
+    case PENDING:
+        status = "pending"
+    }
+    return status
+}
+
+func (t *TaskContent) deleteTask() {
+	/*
+	   TODO: logic to delete an existing task by ID. Check if the task exist
+       first before deleting, and if it doesn't I should tell the user.
+	*/
+}
+
+func (t *TaskContent) updateTask() {
+	/*
+	   TODO: logic to update the status of a task and change the 
+       'UpdatedAt' time. It should Unmarshall and check the exist tasks 
+       in the JSON file first.
+       If the task doesn't exist I should inform the user.
+	*/
+}
+/*
+    TODO: When I add a new task I should load previous tasks first and append 
+    the new one in order from oldest to newest.
+*/
+func addTask(newTask Task) {
     currentTime := time.Now().Format(time.ANSIC)
-	task := []Task{{
-		Id:          1,
+	var newId TaskId = TaskId(rand.Intn(100))
+	newTask[newId] = TaskContent{
 		Description: "Test text for my json.",
-		Status:      IN_PROGRESS,
+		Status:      checkTaskStatus(IN_PROGRESS),
 		CreatedAt:   currentTime,
 		UpdatedAt:   currentTime,
-	}}
+	}
 
-	byteValue, err := json.Marshal(task)
+	byteValue, err := json.Marshal(newTask)
 	if err != nil {
 		fmt.Printf("Failed to create JSON: %s\n", err)
 		return
@@ -59,4 +77,9 @@ func main() {
 	if err != nil {
 		fmt.Printf("Failed to write JSON file: %s\n", err)
 	}
+}
+
+func main() {
+    task := make(Task)
+	addTask(task)
 }
